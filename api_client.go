9ea5dc965871cc6738d559755d655af2aa6df612
DmitryD <dima.dyachkov@gmail.com>
2022-07-15 01:54:31 +0500

    Updated client

--- api_client.go
+++ api_client.go
@@ -1,9 +1,10 @@
 package fireblocksdk
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 
 	"github.com/pkg/errors"
@@ -12,15 +13,15 @@ import (
 )
 
 type IAPIClient interface {
-	DoPostRequest(path string, body interface{}) (*http.Response, error)
-	DoGetRequest(path string, body interface{}) (*http.Response, error)
-	DoPutRequest(path string, body interface{}) (*http.Response, error)
-	DoDeleteRequest(path string, body interface{}) (*http.Response, error)
+	DoPostRequest(path string, body interface{}) ([]byte, int, error)
+	DoGetRequest(path string, body interface{}) ([]byte, int, error)
+	DoPutRequest(path string, body interface{}) ([]byte, int, error)
+	DoDeleteRequest(path string, body interface{}) ([]byte, int, error)
 }
 
 type APIClient struct {
-	client *retryablehttp.Client
-	auth   IAuthProvider
+	httpClient *retryablehttp.Client
+	auth       IAuthProvider
 }
 
 func NewAPIClient(auth IAuthProvider) *APIClient {
@@ -29,33 +30,69 @@ func NewAPIClient(auth IAuthProvider) *APIClient {
 	return &APIClient{client, auth}
 }
 
-func (client *APIClient) DoPostRequest(path string, body interface{}) (*http.Response, error) {
+func (api *APIClient) makeRequest(method, path string, body interface{}) ([]byte, int, error) {
+	var status = http.StatusInternalServerError
 	bodyJson, err := json.Marshal(body)
 	if err != nil {
-		return nil, errors.Wrap(err, "failed to marshal body")
+		return nil, status, errors.Wrap(err, "failed to marshal body")
 	}
 
-	jwtToken, err := client.auth.SignJwt(path, bodyJson)
+	jwtToken, err := api.auth.SignJwt(path, bodyJson)
 	if err != nil {
-		return nil, errors.Wrap(err, "failed to sign request")
+		return nil, status, errors.Wrap(err, "failed to sign request")
 	}
 
-	req, _ := http.NewRequest("POST", path, bytes.NewBuffer(bodyJson))
-	req.Header.Add("X-API-Key", client.auth.GetApiKey())
+	req, err := retryablehttp.NewRequest(method, path, bodyJson)
+	if err != nil {
+		return nil, status, errors.Wrap(err, "failed to create request")
+	}
+
+	req.Header.Add("X-API-Key", api.auth.GetApiKey())
 	req.Header.Add("Authorization", fmt.Sprintf(`Bearer %s`, jwtToken))
 	req.Header.Set("Content-Type", "application/json; charset=UTF-8")
 
-	return nil, nil
+	resp, err := api.httpClient.Do(req)
+	if err != nil {
+		return nil, status, err
+	}
+
+	status = resp.StatusCode
+	log.Printf(
+		"[%s] Path %s: status: %s, code: %d, content-length: %d",
+		method,
+		path,
+		resp.Status,
+		resp.StatusCode,
+		resp.ContentLength,
+	)
+
+	//goland:noinspection GoUnhandledErrorResult
+	defer resp.Body.Close()
+
+	var result []byte
+	if resp != nil {
+		responseBody, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return result, status, errors.Wrap(err, "failed to parse response body")
+		}
+		result = responseBody
+	}
+
+	return result, status, nil
 }
 
-func (client *APIClient) DoGetRequest(path string, body interface{}) (*http.Response, error) {
-	return nil, nil
+func (api *APIClient) DoPostRequest(path string, body interface{}) ([]byte, int, error) {
+	return api.makeRequest("POST", path, body)
 }
 
-func (client *APIClient) DoPutRequest(path string, body interface{}) (*http.Response, error) {
-	return nil, nil
+func (api *APIClient) DoGetRequest(path string, body interface{}) ([]byte, int, error) {
+	return api.makeRequest("GET", path, body)
 }
 
-func (client *APIClient) DoDeleteRequest(path string, body interface{}) (*http.Response, error) {
-	return nil, nil
+func (api *APIClient) DoPutRequest(path string, body interface{}) ([]byte, int, error) {
+	return api.makeRequest("PUT", path, body)
+}
+
+func (api *APIClient) DoDeleteRequest(path string, body interface{}) ([]byte, int, error) {
+	return api.makeRequest("DELETE", path, body)
 }
